a5e680870ddb81da0b1432319e8a049249bddaf6
roney <roneymonte@gmail.com>
2014-09-08 11:53:03 -0300

    tentando re-estabelecer de alguma forma os arquivos originais.

---
+++ helloWorld_piscaLed/src/helloWorld_piscaLed.c
@@ -0,0 +1,57 @@
+/*
+===============================================================================
+ Name        : helloWorld_piscaLed.c
+ Author      : Roney
+ Version     :
+ Copyright   : GPL
+ Description : main definition
+===============================================================================
+
+Baseado no exemplo do Livro Miyadaira, capitulo 8.1 na pagina 249
+*/
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+#include "SysTick_Lib.h"
+
+// TODO: insert other definitions and declarations here
+//#define C
+
+int main(void) {
+
+    // TODO: insert code here
+	SystemInit();	// inicia o sistema e clock conforme "system_LPC17xx.c" no CMSIS
+	Config_SysTick_ms();	// fiz pequena modificacao na biblioteca do Miyadaira para suportar milisegundos
+
+	LPC_PINCON->PINSEL1	&=	~(0b11<<12);	// configura pino P0.22 como GPIO
+	LPC_GPIO0->FIODIR 	|=	1<<22;			// configura pino P0.22 como saida
+
+	LPC_GPIO0->FIOSET |=	1<<22;
+	LPC_GPIO0->FIOCLR |=	1<<22;
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+
+        LPC_GPIO0->FIOPIN |=	1<<22;		// liga o pino 0.22
+        delay_ms(1000);					// 1/2 seg pausa
+
+        LPC_GPIO0->FIOPIN &=	~(1<<22);	// desliga o pino 0.22
+        delay_ms(1000);					// 1/2 seg pausa
+
+        LPC_GPIO0->FIOSET |=	1<<22;		// liga (SET) o pino 0.22
+        delay_ms(2000);					// 1 seg pausa
+
+        LPC_GPIO0->FIOCLR |=	1<<22;		// desliga (CLR) o pino 0.22
+        delay_ms(2000);					// 1 seg pausa
+
+    }
+    return 0 ;
+}
